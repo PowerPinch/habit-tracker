5a013acb60d0e38f12f6d8c4924de6e16bb92e5a
Jonathan Brune <jondbrune@hotmail.com>
2018-01-15 20:56:49 -0600

    Add PUT request handler, genericize parseRequests

--- server/index.js
+++ server/index.js
@@ -1,23 +1,42 @@
+const R = require('ramda')
+
 const http = require('http')
 const uuid = require('uuid/v4')
-const itemList = []
+let itemList = []
+
+const parseRequest = (req, res, callback) => {
+  let payload = ''
+  req.setEncoding('utf8')
+  req.on('data', chunk => {
+    payload += chunk
+  })
+  req.on('end', () => {
+    try {
+      const body = JSON.parse(payload)
+      callback(body)
+    } catch (e) {
+      res.writeHead(400)
+      res.end()
+    }
+  })
+}
 const server = http.createServer((req, res) => {
   switch (req.method) {
-    case 'POST':
-      let payload = ''
-      req.setEncoding('utf8')
-      req.on('data', chunk => {
-        payload += chunk
+    case 'PUT':
+      const itemId = req.url.slice(1)
+      parseRequest(req, res, ({ item: newItem }) => {
+        itemList = R.map(
+          ({ item, id }) =>
+            id === itemId ? { item: newItem, id } : { item, id },
+          itemList
+        )
+        res.end()
       })
-      req.on('end', () => {
-        try {
-          let { item } = JSON.parse(payload)
-          itemList.push({ item, id: uuid() })
-        } catch (e) {
-          res.writeHead(400)
-        } finally {
-          res.end()
-        }
+      break
+    case 'POST':
+      parseRequest(req, res, ({ item }) => {
+        itemList.push({ item, id: uuid() })
+        res.end()
       })
       break
     case 'GET':
