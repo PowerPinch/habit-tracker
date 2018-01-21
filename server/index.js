5aae86a054ec7faf49ed7c0eacc802bee51f692b
Jonathan Brune <jondbrune@hotmail.com>
2018-01-20 18:10:51 -0600

    Fix #3

--- server/index.js
+++ server/index.js
@@ -16,12 +16,18 @@ app.post('/', (req, res) => {
 })
 app.put('/:itemId', (req, res) => {
   const { item: newItem } = req.body
-  itemList = R.map(
-    ({ item, id }) =>
-      id === req.params.itemId ? { item: newItem, id } : { item, id },
-    itemList
-  )
-  res.end()
+  const itemHasId = R.propEq('id', req.params.itemId)
+  const index = R.findIndex(itemHasId, itemList)
+  if (index < 0) {
+    res.status(404).end()
+  } else {
+    itemList = R.update(
+      index,
+      { item: newItem, id: req.params.itemId },
+      itemList
+    )
+    res.end()
+  }
 })
 app.delete('/:itemId', (req, res) => {
   itemList = R.reject(({ item, id }) => id === req.params.itemId, itemList)
